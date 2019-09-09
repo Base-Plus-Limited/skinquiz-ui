35fbd000001d0db4a35b7b1356a2b701b10a9815
Chima Nwosu <me@chimannwosu.com>
2019-09-09 22:21:24 +0100

    Moved static files functions into production if statement

--- server.ts
+++ server.ts
@@ -20,15 +20,15 @@ class App {
   }
 
   private config () {
-    this.express.use(express.static(__dirname + '/build'));
-    this.express.use(express.static(__dirname + '/build/static/'));
     this.express.use((req, res, next) => {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
-
+    
     if (process.env.NODE_ENV === 'production') {
+      this.express.use(express.static(__dirname + '/build'));
+      this.express.use(express.static(__dirname + '/build/static/'));
       this.express.get('/', (req: Request, res: Response) => {
         res.sendFile(join(__dirname, '/build', 'index.html'));
       });
