149aaa1aa3fea7b6decb68c27f494b89188a7d6f
Chima Nwosu <me@chimannwosu.com>
2019-09-12 20:57:59 +0100

    Stripped more

--- server.ts
+++ server.ts
@@ -20,22 +20,22 @@ class App {
   }
 
   private config () {
-    this.express.use(express.static(__dirname + '/build'));
-    this.express.use(express.static(__dirname + '/build/static/'));
-    this.express.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+    // this.express.use(express.static(__dirname + '/build'));
+    // this.express.use(express.static(__dirname + '/build/static/'));
+    // this.express.use((req, res, next) => {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //   next();
+    // });
 
-    if (process.env.NODE_ENV === 'production') {
-      this.express.get('/', (req: Request, res: Response) => {
-        res.sendFile(join(__dirname, '/build', 'index.html'));
-      });
-      this.express.get('/api', (req: Request, res: Response) => {
-        res.sendFile(join(__dirname, '/server.js'));
-      });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   this.express.get('/', (req: Request, res: Response) => {
+    //     res.sendFile(join(__dirname, '/build', 'index.html'));
+    //   });
+    //   this.express.get('/api', (req: Request, res: Response) => {
+    //     res.sendFile(join(__dirname, '/server.js'));
+    //   });
+    // }
   }
 
   private mountRoutes (): void {
