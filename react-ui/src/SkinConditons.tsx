6b69e492bf2f90f4e09e97aae559b4c12d1d3876
Chima Nwosu <me@chimannwosu.com>
2019-09-30 20:29:40 +0100

    Updated skin condition enums

--- react-ui/src/SkinConditons.tsx
+++ react-ui/src/SkinConditons.tsx
@@ -1,11 +1,11 @@
 const SkinConditionEnums: { [key: string]: string } = {
-  "03": "Very Dry to Dry",
+  "03": "Very Dry or Dry",
   "04": "Dry Combination",
   "05": "Dry Combination",
   "15": "Combination Oily",
-  "14": "Combination Oily",
+  "14": "Normal",
   "13": "Dry Combination",
-  "25": "Oily",
+  "25": "Very Oily or Oily",
   "24": "Combination Oily",
   "23": "Combination Oily",
 }
