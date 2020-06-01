19db74d21d054a61f86b89a15e4575cf55a1cd32
Chima Nwosu <me@chimannwosu.com>
2020-06-01 12:43:10 +0100

    Refactor: Restyled summary page

--- react-ui/src/Components/Shared/Image.tsx
+++ react-ui/src/Components/Shared/Image.tsx
@@ -6,22 +6,14 @@ export interface ImageProps {
   alt: string;
   width?: number;
   isWelcomeScreen?: boolean;
-  isSummaryScreen?: boolean;
 }
  
-const StyledImage: React.SFC<ImageProps> = ({alt, src, width, isSummaryScreen}) => (
-  <Image isSummaryScreen={isSummaryScreen} isWelcomeScreen width={width} src={src} alt={alt}></Image>
+const StyledImage: React.SFC<ImageProps> = ({alt, src, width}) => (
+  <Image isWelcomeScreen width={width} src={src} alt={alt}></Image>
 )
  
 const Image = styled.img`
   max-width: ${(props: ImageProps) => props.isWelcomeScreen ? '95%' : '100%'};
-  ${props => props.isSummaryScreen && `
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-top: -7.5px;
-    margin-left: -7.5px;
-  `}
 `;
 
 export default StyledImage;
