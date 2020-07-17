adbdc86d554de866c67074692fd154e8154558e3
Chima Nwosu <me@chimannwosu.com>
2020-07-17 09:38:25 +0100

    Updated button to resolve errors

--- react-ui/src/Components/Button.tsx
+++ react-ui/src/Components/Button.tsx
@@ -25,15 +25,29 @@ const Button = styled.button`
   font-weight: 600;
 `;
 
-const StyledBackButton = styled(Button)`
+const StyledBackButton = styled.button`
   color: ${props => props.theme.brandColours.basePink};
+  border: solid 2px ${props => props.theme.brandColours.basePink};
   margin: -3px 0 0 0;
   padding: 3px 10px;
-  border: solid 2px ${props => props.theme.brandColours.basePink};
+  font-family: ${props => props.theme.subHeadingFont};
+  font-weight: 600;
+  cursor: pointer;
+  text-transform: uppercase;
+  background: none;
+  outline: none;
 `
 
-const StyledSummaryButton = styled(Button)`
+const StyledSummaryButton = styled.button`
   margin: ${(props: ButtonProps) => props.addMargin ? "0 15px 0" : "0 auto"};
+  font-family: ${props => props.theme.subHeadingFont};
+  font-weight: 600;
+  cursor: pointer;
+  text-transform: uppercase;
+  padding: 10px 15px;
+  background: none;
+  border: solid 1px ${props => props.theme.brandColours.baseDarkGreen};
+  outline: none;
 `
 
 export { StyledButton, StyledBackButton, StyledSummaryButton };
