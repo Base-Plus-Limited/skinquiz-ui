4adca517582684764cac98873d1680c587fce0e3
Chima Nwosu <me@chimannwosu.com>
2021-03-16 18:09:08 -0400

    Remove server

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import App from './server';
-const app = new App().express;
-const port = process.env.PORT||3001;
-app.listen(port, () => {
-  console.log(`Server is listening on ${port}`)
-});
-process.on('uncaughtException', (err) => {
-  console.error(`ERROR: ${err.message}`)
-})
